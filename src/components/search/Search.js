d97ef1b5a0d680cd83bf72313db01606c0c19df8
daria-udrmm <Darya.Vi@yandex.ru>
2022-02-20 13:49:57 +0300

    change popup reducer

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -16,6 +16,7 @@ const Search = () => {
         e.preventDefault();
         dispatch(activeFilteredChange(({title: text})));
     }
+    
 
     useEffect(() => {
         setText('');
--- src/reducer/popup.js
+++ src/reducer/popup.js
@@ -8,11 +8,13 @@ const popup = (state = initialState, action) => {
     switch (action.type) {
         case 'POPUP_SHOW':
             return {
+                ...state,
                 activePopup: true,
                 popupInfo: action.payload
             }
         case 'POPUP_HIDE':
             return {
+                ...state,
                 activePopup: false,
                 popupInfo: {}
             }
